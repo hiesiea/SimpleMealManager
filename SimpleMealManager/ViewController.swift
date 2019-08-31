5dc0df2bfc82a1edf3bc2cdda2e6d0305924fc9d
hiesiea <hiesiea@gmail.com>
2019-08-31 19:12:56 +0900

    Initial Commit

---
+++ SimpleMealManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMealManager
+//
+//  Created by Hitoshi KAMADA on 2019/08/31.
+//  Copyright © 2019 hitoshi.kamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleMealManagerTests/SimpleMealManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleMealManagerTests.swift
+//  SimpleMealManagerTests
+//
+//  Created by Hitoshi KAMADA on 2019/08/31.
+//  Copyright © 2019 hitoshi.kamada. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleMealManager
+
+class SimpleMealManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
