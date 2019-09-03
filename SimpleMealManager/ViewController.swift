078e72ea6ceef6da57e4470eb699e6b26a832fce
hiesiea <hiesiea@gmail.com>
2019-09-03 21:25:51 +0900

    カメラ撮影機能追加。

--- SimpleMealManager/ViewController.swift
+++ SimpleMealManager/ViewController.swift
@@ -6,15 +6,76 @@
 //  Copyright © 2019 hitoshi.kamada. All rights reserved.
 //
 
+import ESTabBarController
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    private let homeViewController: UIViewController? = {
+        // タブをタップした時に表示するViewControllerを設定する
+        let homeStoryBoard: UIStoryboard = UIStoryboard(name: "Home", bundle: nil)
+        let viewController = homeStoryBoard.instantiateInitialViewController()
+        return viewController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTab()
     }
-
-
+    
+    private func setupTab() {
+        // 画像のファイル名を指定してESTabBarControllerを作成する
+        let tabBarController: ESTabBarController! = ESTabBarController(tabIconNames: ["home", "photo"])
+        
+        // 背景色、選択時の色を設定する
+        tabBarController.selectedColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        tabBarController.buttonsBackgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        tabBarController.selectionIndicatorHeight = 3
+        
+        // 作成したESTabBarControllerを親のViewController（＝self）に追加する
+        addChild(tabBarController)
+        let tabBarView = tabBarController.view!
+        tabBarView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tabBarView)
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tabBarView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tabBarView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tabBarView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tabBarView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            ])
+        tabBarController.didMove(toParent: self)
+        
+        tabBarController.setView(homeViewController, at: 0)
+        
+        // 真ん中のタブはボタンとして扱う
+        tabBarController.highlightButton(at: 1)
+        tabBarController.setAction({
+            // ボタンが押されたらカメラを指定してピッカーを開く
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                let pickerController = UIImagePickerController()
+                pickerController.delegate = self
+                pickerController.sourceType = .camera
+                self.present(pickerController, animated: true, completion: nil)
+            }
+            
+        }, at: 1)
+    }
+    
+    // 写真を撮影/選択したときに呼ばれるメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if info[.originalImage] != nil {
+            // 撮影された画像を取得する
+            let image = info[.originalImage] as! UIImage
+            
+            // 画像を登録
+            
+            // 閉じる
+            picker.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // 閉じる
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
-
