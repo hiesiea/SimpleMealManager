3e4326c7738e5840ddb463d5759c2edab46c3e51
hiesiea <hiesiea@gmail.com>
2019-09-06 20:19:42 +0900

    画面の準備。

---
+++ SimpleMealManager/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  SimpleMealManager
+//
+//  Created by Hitoshi KAMADA on 2019/09/06.
+//  Copyright © 2019 hitoshi.kamada. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
